6cd8986c0855b7be60f0e456150cf04cc84cbedc
StFavn <St.favn@yandex.ru>
2021-11-22 23:35:13 +0700

    make migrations

--- uploader/models.py
+++ uploader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class Image(models.Model):
-    title = models.CharField(max_length=200, default=uuid.uuid4())
+    title = models.CharField(max_length=200, default=uuid.uuid4)
     image = models.ImageField(blank=False, upload_to='images/')
     
     def __str__(self):
